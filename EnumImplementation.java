adc964000594fea9c6db9f57ad227af107df286d
Harshita Panwar <149288907+harshitaapanwar@users.noreply.github.com>
2025-03-11 12:26:52 +0530

    Add files via upload

---
+++ EnumImplementation.java
@@ -0,0 +1,14 @@
+package javaConcept;
+
+
+enum Status{
+	RUNNING,FAILED,PENDING,SUCCES
+}
+public class EnumImplementation {
+  public static void main(String args[]) {
+	  
+	  Status s = Status.RUNNING;
+	  System.out.println(s);
+  
+  }
+}
